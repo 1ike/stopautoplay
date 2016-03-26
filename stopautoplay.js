ecca7b546b3dfe066218f4e014d010eade7e83ee
discodanser <googlohren@gmail.com>
2016-03-27 00:34:34 +0300

    Single video page also can paused.

--- stopautoplay.js
+++ stopautoplay.js
@@ -1,17 +1,22 @@
 // Stop autoplay on youtube page (for Firefox with Greasemonkey) 
 
 
+// For single video page
+var player = document.getElementsByTagName('VIDEO')[0];
+if (player) player.oncanplay = pauseVideo;
+
+// For user page
 window.addEventListener("DOMNodeInserted", function (e) {
-  
    if (e.target.tagName == 'VIDEO') {
-
     player = e.target;
-    
-    player.oncanplay = function () {
-      setTimeout (function() { 
-        player.pause();
-      }, 0);
-    }
+    player.oncanplay = pauseVideo;
    }
-
 });
+
+
+function pauseVideo () {
+  var self = this;
+  setTimeout (function() { 
+   self.pause();
+  }, 0);
+}
