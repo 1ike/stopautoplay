b9e2a2b8d1360f2a6ba728a72b5aadccb2329805
1ike <googlohren@gmail.com>
2017-12-26 01:21:51 +0300

    Fix after youtube renew.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Stop autoplay at youtube page.
 
 
-**For using in Mozilla Firefox with Greasemonkey plugin.**
+**For using in Mozilla Firefox with Greasemonkey|Tampermonkey plugin.**
 
-Stop autoplay on 
+Stop autoplay on
 
-    http*://www.youtube.com/*   (Use it in Greasemonkey with  // @include )
-  
+    http*://www.youtube.com/*
+
 
 * Work at user page and at single video page.
 * Not preventing buffering only pause video at start.
--- stopautoplay.js
+++ stopautoplay.js
@@ -1,24 +1,15 @@
-// Stop autoplay on youtube page (for Firefox with Greasemonkey) 
+// Stop autoplay on youtube page (for Firefox with Greasemonkey/Tampermonkey) 
 
 
-// For single video page
+// For single video page  (comment these lines if need not)
 var player = document.getElementsByTagName('VIDEO')[0];
-if (player)  player.oncanplay = pauseVideo;
+if (player)  player.oncanplay = () => player.pause();
 
 
-// For user page
+// For user page  (comment these lines if need not)
 window.addEventListener("DOMNodeInserted", function (e) {
-   if (e.target.tagName == 'VIDEO') {
+   if (e.target.tagName == 'VIDEO' && document.querySelector('.branded-page-box VIDEO')) {
       player = e.target;
-      player.oncanplay = pauseVideo;
+      player.oncanplay = () => player.pause();
    }
 });
-
-
-function pauseVideo () {
-  var self = this;
-  setTimeout (function() {
-     self.pause();
-     self.oncanplay = null;
-  }, 0);
-}
