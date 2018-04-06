8186972d7c09a12e3a986e5398bfbc9a03e266a2
1ike <googlohren@gmail.com>
2018-04-06 19:34:30 +0300

    Actual fixes and add  Autoplay on fullscreen.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Stop autoplay on
     http*://www.youtube.com/*
 
 
-* Work at home user page and at single video page (noly when open new tab or new window, not for links inside). 
+* Work only when open new tab or new window or refresh, not for links inside.
+* Work at home user page and at single video page (for single video page you need change condition in line #18 in accord line #17).
 * Not preventing buffering only pause video at start.
--- stopautoplay.js
+++ stopautoplay.js
@@ -1,37 +1,67 @@
 // ==UserScript==
 // @name        YouTube Stop Autoplay
 // @namespace   youtube
-// @version     0.0.4
-// @description Stop autoplay on youtube page (for Firefox with Greasemonkey|Tampermonkey) 
+// @version     0.0.5
+// @description Stop autoplay on youtube page (for Firefox with Greasemonkey|Tampermonkey)
 // @include     http*://www.youtube.com/*
 // @grant       me
 // ==/UserScript==
 
+(function () {
+  const target = document.body;
 
-const target = document.body;
+  const observer = new MutationObserver(function (mutations) {
 
-const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function (mutation) {
+      /* // for single video page either
+      if (mutation.target.id == 'c4-player' || mutation.target.id == 'movie_player') { */
+      if (mutation.target.id == 'c4-player') {
+        const player = target.querySelector('video');
+        if (player) {
+          player.onloadeddata = () => {
+            player.pause();
+            // player.onloadeddata = null;
+          };
 
-    mutations.forEach(function(mutation) {
-        if (mutation.target.id == 'movie_player' || mutation.target.id == 'player-container') {
-
-            player = target.querySelector('video');
-
-            player.onloadeddata = () => {
-                player.pause();
-                player.onloadeddata = null;
-            };
-
-            observer.disconnect();
+          observer.disconnect();
         }
+      }
     });
-});
+  });
 
-// конфигурация нашего observer:
-const config = { childList: true, subtree: true };
+  // конфигурация нашего observer:
+  const config = {
+    childList: true,
+    subtree: true
+  };
 
-// передаём в качестве аргументов целевой элемент и его конфигурацию
-observer.observe(target, config);
+  // передаём в качестве аргументов целевой элемент и его конфигурацию
+  observer.observe(target, config);
 
-// позже можно остановить наблюдение
-//observer.disconnect();
+  // позже можно остановить наблюдение
+  //observer.disconnect();
+
+})();
+
+
+/*
+// Autoplay on fullscreen
+
+(function () {
+  const playOnFullscreen = function (event) {
+    const fullscreenElement = document.fullscreenElement ||
+      document.mozFullScreenElement ||
+      document.webkitFullscreenElement ||
+      msFullscreenElement;
+
+    if (fullscreenElement) {
+      const player = fullscreenElement.querySelector('video');
+      player.play();
+    }
+  };
+  document.addEventListener("fullscreenchange", playOnFullscreen);
+  document.addEventListener("mozfullscreenchange", playOnFullscreen);
+  document.addEventListener("webkitfullscreenchange", playOnFullscreen);
+  document.addEventListener("onmsfullscreenchange", playOnFullscreen);
+})();
+ */
